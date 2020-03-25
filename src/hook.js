2bb4a93a7614f890fed482e551ac3d0999f17deb
Wallysson Nunes <wally.nm@gmail.com>
2020-03-25 18:00:26 -0300

    Added new check for Hooks

--- src/hook.js
+++ src/hook.js
@@ -1,10 +1,23 @@
 import React, { useEffect, useContext, useCallback } from 'react'
 import ContextExperiment from './ContextExperiment'
 
+export const useExperimentContext = () => {
+  const ctx = useContext(ContextExperiment)
+
+  if (process.env.NODE_ENV !== 'production' && !ctx) {
+    throw new Error(
+      'could not find react-experiment context value; please ensure the component is wrapped in a <Provider>'
+    )
+  }
+
+  return ctx
+}
+
 export const useEmitter = (nameExperiment, variablesToTest) => {
-  const contextExperiment = useContext(ContextExperiment)
+  const ctx = useExperimentContext()
+  console.info(ContextExperiment)
   const execute = useCallback(() => {
-    const { experiments, method } = contextExperiment
+    const { experiments, method } = ctx
 
     console.log('nameExperiment', nameExperiment)
     console.log('experiments[nameExperiment]', experiments[nameExperiment])
